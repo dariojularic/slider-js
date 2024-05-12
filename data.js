8f12c00bbc04c62f3ed044bc8d4151a9feb07c80
Dario Jularic <dariojularic0@gmail.com>
2024-05-12 21:43:43 +0200

    Add more data

--- data.js
+++ data.js
@@ -8,5 +8,35 @@ array = [
             image: "",
             location: "Zagreb"
         }
+    },
+    {
+        id: 2,
+        heading: "neki naslov",
+        paragraph: "neki paragraph",
+        user: {
+            name: "Pero",
+            image: "",
+            location: "Paris"
+        } 
+    },
+    {
+        id: 3,
+        heading: "neki naslov",
+        paragraph: "neki paragraph",
+        user: {
+            name: "Marija",
+            image: "",
+            location: "Pula"
+        }
+    },
+    {
+        id: 4,
+        heading: "neki naslov",
+        paragraph: "neki paragraph",
+        user: {
+            name: "Kruno",
+            image: "",
+            location: "Beograd"
+        }
     }
 ]
