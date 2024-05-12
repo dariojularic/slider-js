adc7d19cd68b3f0fecff1f7aeaac3ce3266c7d38
Dario Jularic <dariojularic0@gmail.com>
2024-05-12 21:37:03 +0200

    Create data

---
+++ data.js
@@ -0,0 +1,12 @@
+array = [
+    {
+        id: 1,
+        heading: "neki naslov",
+        paragraph: "neki paragraph",
+        user: {
+            name: "Dario",
+            image: "",
+            location: "Zagreb"
+        }
+    }
+]
